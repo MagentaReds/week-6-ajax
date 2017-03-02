2aee2ea1408521425b3c9c7e930df620cf4b5015
MagentaReds <grantodaniels@gmail.com>
2017-03-01 23:49:35 -0600

    Added comments

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -1,11 +1,16 @@
+//Initial topic's to display
 var searchTopics = ["Chewbacca", "Han Solo", "Luke Skywalker", "Snoke", "Ahsoka Tano", "Revan"];
 
+//some global variables
 var baseUrl="https://api.giphy.com/v1/gifs/search";
+//public giphy api key
 var apiKey="dc6zaTOxFJmzC";
+//# of images our ajax calls requests for the search
 var imageLimit=10;
 var baseParam="?"+"api_key="+apiKey+"&limit="+imageLimit+"&q=";
 var buttonCounter=0;
 
+//takes a string, adds a button to the button div based on that string
 function addButton(topic) {
   var ele = $("<button>");
   ele.addClass("btn btn-success topic-button");
@@ -19,11 +24,14 @@ function addButton(topic) {
   ++buttonCounter;
 }
 
+//just pushes a new topic to the topic array and calls addButton(topic)
 function addTopic(topic) {
   searchTopics.push(topic);
   addButton(topic);
 }
 
+//takes the results array from the giphy api ajax response, displays the images
+//and its ratings in bootstrap wells
 function displayImages(array) {
   $("#results-display").empty();
 
@@ -50,6 +58,7 @@ function displayImages(array) {
 
 }
 
+//remakes all the buttons for each entry in the topic array, this is only called once or twice.
 function redisplayButtons() {
   buttonCounter=0;
   $("#buttons-display").empty();
@@ -59,6 +68,7 @@ function redisplayButtons() {
 
 }
 
+//actualy ajax request code, calls displayImages() in the promise with the response data
 function getAjaxResponse(topic) {
   $.ajax({
     url: baseUrl+baseParam+topic,
@@ -69,20 +79,27 @@ function getAjaxResponse(topic) {
 
 }
 
+//Start of logic code, rather than function/variable definitions
 $(document).ready(function(){
 
+  //initial populate buttons based on topic array
   redisplayButtons();
 
+  //takes user input from text box, adds topic to array, and makes new button on submit click
   $("#search-submit").on("click", function(){
     var search=$("#search-terms").val().trim();
     addTopic(search);
   });
 
+  //instead of binding an event listener to each button, just let the document itself
+  //listen for clicks on .topic-buttons to start the ajax request from giphy
   $(document).on("click", ".topic-button", function(event) {
     var data = $(this).attr("data-topic");
     getAjaxResponse(data);
   });
 
+  //similar to the above, but with .result-image
+  //click the image to switch between still and animated
   $(document).on("click", ".result-image", function(event) {
     var state = $(this).attr("data-state");
     if(state==="still"){
